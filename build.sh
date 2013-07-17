cdfc49b932aff392f7321c6d3e61cd0ea7aacf48
starthomas610 <stmk93610@gmail.com>
2013-07-16 18:10:04 -0700

    Build for x86

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ V8_SRC_ROOT_DEFAULT=./support/v8
 NUM_CPUS=1
 PLATFORM_VERSION=android-8
 ANT_TARGET=debug
-V8_TARGET=android_arm.release
+ARCHITECTURES="arm,x86"
 INSTALL=false
 DEBUG=false
 
@@ -18,18 +18,19 @@ This script builds v8 against the Android NDK and a sample project skeleton that
 Options:
   -h                  Show this help message and exit
   -s <v8_src>         The path to v8's project sourcetree's root. (default $V8_SRC_ROOT_DEFAULT)
-  -v <v8_target>      Build target for v8. (default android_arm.release)
+  -a <archs>          Comma-separated build architectures.
+                        Available architectures: arm, x86. (default $ARCHITECTURES)
   -n <ndk_dir>        The path to the Android NDK. (default \$ANDROID_NDK_ROOT)
   -t <toolchain_dir>  The path to the Android's toolchain binaries. (default \$ANDROID_TOOLCHAIN)
-  -p <platform>       The Android SDK version to support (default $V8_TARGET)
+  -p <platform>       The Android SDK version to support (default $PLATFORM_VERSION)
   -j <num-cpus>       The number of processors to use in building (default $NUM_CPUS)
   -i                  Install resulting example APK onto default device.
   -d                  Install resulting example APK and begin debugging via ndk-gdb. (implicitly sets -i)
 EOF
 }
 
-red='\E[31;1m'
-green='\E[32;3m'
+red='\e[31;1m'
+green='\e[32;3m'
 
 function msg() {
     echo -n -e "$green"
@@ -59,7 +60,7 @@ function checkForErrors() {
     fi
 }
 
-while getopts "hs:v:n:t:p:j:id" OPTION; do
+while getopts "hs:a:n:t:p:j:id" OPTION; do
   case $OPTION in
     h)
       usage
@@ -68,8 +69,8 @@ while getopts "hs:v:n:t:p:j:id" OPTION; do
     s)
       V8_SRC_ROOT=$OPTARG
       ;;
-    v)
-      V8_TARGET=$OPTARG
+    a)
+      ARCHITECTURES=$OPTARG
       ;;
     n)
       ANDROID_NDK_ROOT=$OPTARG
@@ -115,39 +116,76 @@ then
   exit
 fi
 
-msg Building v8 for android target...
+msg "Building v8..."
 pushd $V8_SRC_ROOT
+
 if [ ! -d "./build/gyp" ]
 then
+  msg "Building GYP first"
   make dependencies -j$NUM_CPUS
 fi
-make $V8_TARGET -j$NUM_CPUS
-checkForErrors
+
+# Iterate over archs and build v8
+for i in $(echo $ARCHITECTURES | tr "," "\n")
+do
+  TARGET=""
+  if [ "$i" == "arm" ]; then
+    TARGET="android_arm.release"
+  elif [ "$i" == "x86" ]; then
+    TARGET="android_ia32.release"
+  fi
+  make $TARGET -j$NUM_CPUS
+  checkForErrors "Error building v8 (target=$TARGET)"
+done
+
 popd
 
-msg Copying static library files... 
-mkdir -p support/android/libs
-rsync -tr $V8_SRC_ROOT/out/$V8_TARGET/obj.target/tools/gyp/*.a support/android/libs/.
-checkForErrors
 
-msg Copying v8 header files...
+msg "Copying static library files..."
+mkdir -p support/android/libs
+for i in $(echo $ARCHITECTURES | tr "," "\n")
+do
+  ARCH=""
+  if [ "$i" == "arm" ]; then
+    ARCH="android_arm.release"
+  elif [ "$i" == "x86" ]; then
+    ARCH="android_ia32.release"
+  fi
+  rsync -tr $V8_SRC_ROOT/out/$ARCH/obj.target/tools/gyp/*.a support/android/libs/.
+  checkForErrors "Error copying v8 static library (arch=$ARCH)"
+done
+
+
+msg "Copying v8 header files..."
 mkdir -p support/include
 rsync -tr $V8_SRC_ROOT/include/* support/include/.
-checkForErrors
+checkForErrors "Error copying v8 header files"
 
-msg Building NDK libraries...
-NDK_DEBUG=1 $ANDROID_NDK_ROOT/ndk-build -j$NUM_CPUS V=1
-checkForErrors "Problem building JNI module."
 
+msg "Building NDK libraries..."
+ABI=""
+for i in $(echo $ARCHITECTURES | tr "," "\n")
+do
+  if [ "$i" == "arm" ]; then
+    ABI="$ABI armeabi armeabi-v7a"
+  elif [ "$i" == "x86" ]; then
+    ABI="$ABI x86"
+  fi
+  NDK_DEBUG=1 $ANDROID_NDK_ROOT/ndk-build -j$NUM_CPUS V=1 "APP_ABI=$ABI"
+  checkForErrors "Error building NDK libraries (ABI=$ABI)"
+done
+
+msg "Creating jv8-native.jar..."
+rm -fr lib
+mv libs lib
+mkdir -p dist/android/libs
+rm dist/android/libs/jv8-native.jar
+zip -r dist/android/libs/jv8-native.jar lib
+checkForErrors "Error creating jv8-native.jar"
+
+msg "Creating jv8.jar"
+rm -fr lib
 ant dist
-checkForErrors "Problem running 'ant dist'"
+checkForErrors "Error creating jv8.jar"
 
-mkdir -p _dist/android
-cp -r libs _dist/android
-checkForErrors "Problem copying complete binaries to dist"
-
-mkdir -p dist/android
-pushd _dist/android
-tar czf ../../dist/android/jv8_android_arm.tar.gz libs
-checkForErrors "Problem creating dist tarball"
-popd
+msg "Libs are in dist/android/libs"
