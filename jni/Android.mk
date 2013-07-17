4204b1cc32d9090674677080aa960da469d5132f
starthomas610 <stmk93610@gmail.com>
2013-07-16 18:01:07 -0700

    Option to build for multiple architectures

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,13 +1,23 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE	:= v8_base
+LOCAL_MODULE	:= v8_base_arm
 LOCAL_SRC_FILES := ../support/android/libs/libv8_base.arm.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE	:= v8_nosnapshot
+LOCAL_MODULE	:= v8_nosnapshot_arm
 LOCAL_SRC_FILES :=  ../support/android/libs/libv8_nosnapshot.arm.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE	:= v8_base_x86
+LOCAL_SRC_FILES :=  ../support/android/libs/libv8_base.ia32.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE	:= v8_nosnapshot_x86
+LOCAL_SRC_FILES :=  ../support/android/libs/libv8_nosnapshot.ia32.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -15,6 +25,6 @@ LOCAL_MODULE    := jv8
 LOCAL_SRC_FILES := jv8.cpp V8Runner.cpp JNIUtil.cpp $(wildcard $(LOCAL_PATH)/../support/v8/src/*.cpp)
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../support/include
 LOCAL_CFLAGS += -Wall -Wno-unused-function -Wno-unused-variable -O3 -funroll-loops -ftree-vectorize -ffast-math
-LOCAL_STATIC_LIBRARIES := v8_base v8_nosnapshot
+LOCAL_STATIC_LIBRARIES := v8_base_arm v8_nosnapshot_arm v8_base_x86 v8_nosnapshot_x86
 LOCAL_LDLIBS += -L$(SYSROOT)/usr/lib -llog
 include $(BUILD_SHARED_LIBRARY)
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,4 +1,4 @@
-APP_ABI := armeabi armeabi-v7a
+APP_ABI := armeabi armeabi-v7a x86
 APP_PLATFORM := android-15
 APP_STL := gnustl_shared
 #APP_MODULES := v8_base
